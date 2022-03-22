898c463bf46cb5db2462637aeae57db971be5d14
nicknav98 <nicholasnavarro98@gmail.com>
2022-03-22 15:24:58 +0200

    updated script to filter mac addresses: work done by Georgi and Michalis

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ import re
 INTERFACE = 'wlan0'
 
 output = subprocess.check_output("sudo arp-scan --interface=" + INTERFACE + " --localnet" + " | awk '/.*:.*:.*:.*:.*:.*/{print $2}'", shell=True)
-print(output)
+#print(output)
+f = open("output.txt", "a")
+f.write(output)
+resultList = output.split("\n")
+print "\n",resultList, len(resultList)
+f.close()
