e44104ffc9a6d98e29480b748e8a009255ba889a
nicknav98 <nicholasnavarro98@gmail.com>
2022-03-24 13:34:47 +0200

    added paho.publish code, succesfully counts mac addresses and prints message as expected

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import subprocess
 from time import sleep
+import paho.mqtt.publish as publish
 import re
 INTERFACE = 'wlan0'
 
+MQTT_HOST = "172.27.227.5"
+MQTT_PORT = "1883"
+
+
+
+
 output = subprocess.check_output("sudo arp-scan --interface=" + INTERFACE + " --localnet" + " | awk '/.*:.*:.*:.*:.*:.*/{print $2}'", shell=True)
-#print(output)
+print(output)
 f = open("output.txt", "a")
 f.write(output)
 resultList = output.split("\n")
-print "\n",resultList, len(resultList)
+print "\n",resultList[1:], len(resultList)-1
 f.close()
+pplnumber = str(len(resultList) - 1)
+
+
+publish.single("home/mac", pplnumber + " People Are Currently in Embedded Lab", hostname=MQTT_HOST, port=MQTT_PORT)
