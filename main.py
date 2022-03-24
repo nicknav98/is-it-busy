e8f73e0a3bfc510cfe50280222f2ff1aa10fc1d2
nicknav98 <nicholasnavarro98@gmail.com>
2022-03-24 13:40:15 +0200

    added time function to run every 30 seconds

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ MQTT_PORT = "1883"
 
 
 
+def func():
+	output = subprocess.check_output("sudo arp-scan --interface=" + INTERFACE + " --localnet" + " | awk '/.*:.*:.*:.*:.*:.*/{print $2}'", shell=True)
+	print(output)
+	f = open("output.txt", "a")
+	f.write(output)
+	resultList = output.split("\n")
+	print "\n",resultList[1:], len(resultList)-1
+	f.close()
+	pplnumber = str(len(resultList) - 1)
 
-output = subprocess.check_output("sudo arp-scan --interface=" + INTERFACE + " --localnet" + " | awk '/.*:.*:.*:.*:.*:.*/{print $2}'", shell=True)
-print(output)
-f = open("output.txt", "a")
-f.write(output)
-resultList = output.split("\n")
-print "\n",resultList[1:], len(resultList)-1
-f.close()
-pplnumber = str(len(resultList) - 1)
 
+	publish.single("home/mac", pplnumber + " People Are Currently in Embedded Lab", hostname=MQTT_HOST, port=MQTT_PORT)
 
-publish.single("home/mac", pplnumber + " People Are Currently in Embedded Lab", hostname=MQTT_HOST, port=MQTT_PORT)
+def keepRunning():
+	func()
+	sleep(30)
+	keepRunning()
+
+keepRunning()
